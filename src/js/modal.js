f31042bdeaaec2691d320c1355f2e9529de2155b
Sergii <svbulgakov75@gmail.com>
2023-06-18 19:14:59 +0300

    grrg

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,4 +13,26 @@
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
     }
-  })();
+})();
+  
+
+function renderModalWindow(book_image) {
+  const books = JSON.parse(localStorage.getItem("books"));
+  const book = books.find(({ image }) => image === "IMG");
+    const markup = createModalWindow(book);
+  modal.insertAdjacentHTML("beforeend", markup);
+}
+
+function createModalWindow({ title, author, book_image, description }) {
+    return `<div class='modal'> 
+  <div class="picture"><img src='${book_image}' alt='${description}'></div>
+    <div class="info"><h1>${title}>
+  <h2>${author}</h2>
+  <p>${description}</p>
+  <ul class="icons">
+        <li class="icon-link"></li>
+        <li class="icon-link"></li>
+        <li class="icon-link"></li>
+    </ul></div>
+  </div>`;
+}
