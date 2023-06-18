711f25a9c5d9f79c632c5d738df32087f28650d7
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-06-19 00:09:11 +0200

    addCategoryList

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,12 +1,8 @@
-import {fetchBooksCategoryList,
-  fetchTopBooks,
-  fetchBooksByCategory,
-  fetchBookById,} from './serviceApi.js'
+import {fetchBooksCategoryList} from './serviceApi.js'
 
 const category = document.querySelector(".categories-item");
 
-
-category.addEventListener('click', () => {
+function markupList () {
   fetchBooksCategoryList().then(data => {
     const markupCategoriesList = data.map(({list_name }) => {
       return `<li class="categories-list" id="${list_name}">${list_name}</li>`;
@@ -15,17 +11,4 @@ category.addEventListener('click', () => {
   }).catch((error) => console.log(error))
 });
 
-fetchBooksCategoryList();
-
-// async function fetchBooksCategoryList(list_name) {
-//   try {
-//     console.log(123456);
-//     const response = await axios.get(
-      
-//     );
-//     return `<li class="" id="${list_name}">${list_name}</li>`;;
-//   }
-//   catch (error) {
-//       console.log(error);
-//   }
-// };
+markupList();
