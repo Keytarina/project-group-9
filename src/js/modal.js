1c76ec794ba7583a09664d519bdd20c00b31dfb2
Sergii <svbulgakov75@gmail.com>
2023-06-18 09:58:09 +0300

    1

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,28 +1,2 @@
-function imageClickHandler(event) {
-  const image= event.currentTarget.image;
-  if (event.target.nodeName === image) {
-    renderPreview(event.target.image);
-  } 
-}
 
-function renderPreview(image) {
-  const books = JSON.parse(localStorage.getItem("books"));
-  const book = books.find(({ image }) => image === "IMG");
-    const markup = createPreviewMarkup(book);
-  modal.insertAdjacentHTML("beforeend", markup);
-}
-
-function createPreviewMarkup({ title, author, img, text }) {
-    return `<div class='modal'> 
-  <div class="picture"><img src='${img}' alt='${title}'></div>
-    <div class="info"><h1>${title}>
-  <h2>${author}</h2>
-  <p>${text}</p>
-  <ul class="icons">
-        <li class="icon-link"></li>
-        <li class="icon-link"></li>
-        <li class="icon-link"></li>
-    </ul></div>
-  </div>`;
-}
 
