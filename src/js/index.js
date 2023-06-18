15b6eb031bd97cf2cdd9997f25678c1cc5bbef4d
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2023-06-19 00:42:16 +0300

    Add import in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
 // import test from './test.js';
-import './shopping-list';
+import './serviceApi';
+import './authorization';
 import './modal';
+import './header';
+import './support';
+import './categories';
 import './books';
+import './best-sellers';
+import './shopping-list';
+import './shopping-list';
+import './btn-see-more';
