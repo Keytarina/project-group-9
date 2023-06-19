f0d6412583d479a088ab264ec034385ba7164944
lytvynenkoroman84@gmail.com <121231829+RomLyt@users.noreply.github.com>
2023-06-20 00:22:36 +0300

    burger

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,25 +1,24 @@
 const toggleToChangeTheme = document.querySelector('#toggle-button');
 
-function setDarkTheme(){
-    document.body.classList.add('dark')
-    localStorage.theme = 'dark'
+function setDarkTheme() {
+  document.body.classList.add('dark');
+  localStorage.theme = 'dark';
 }
 
-function setLightTheme(){
-    document.body.classList.remove('dark')
-    localStorage.theme = 'light'
+function setLightTheme() {
+  document.body.classList.remove('dark');
+  localStorage.theme = 'light';
 }
 
 toggleToChangeTheme.addEventListener('click', () => {
-   if( document.body.classList.contains('dark')){
-    setLightTheme()
+  if (document.body.classList.contains('dark')) {
+    setLightTheme();
+  } else {
+    setDarkTheme();
+  }
+});
 
-   }else{
-    setDarkTheme()
-   }
-})
-
-if (localStorage.theme === 'dark'){
-    document.body.classList.add('dark')
-    toggleToChangeTheme.checked = true
+if (localStorage.theme === 'dark') {
+  document.body.classList.add('dark');
+  toggleToChangeTheme.checked = true;
 }
