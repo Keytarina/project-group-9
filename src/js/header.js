105c7c1c684c7f8d3411cf186f6045da76e5d077
lytvynenkoroman84@gmail.com <121231829+RomLyt@users.noreply.github.com>
2023-06-20 19:45:05 +0300

    new-wraper

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,7 @@
 document.getElementById('burger').addEventListener('click', function () {
   this.classList.toggle('active');
   document.querySelector('.btn-login').classList.toggle('active');
+  document.querySelector('.burger-wraper').classList.toggle('active');
   document.querySelector('.btn-logout').classList.toggle('active');
   document.getElementById('header-nav').classList.toggle('open');
 });
