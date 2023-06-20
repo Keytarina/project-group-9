5bc37bfca33376671e5a5f5ecd90d1333d724b96
Enab13d <extendeddirection@gmail.com>
2023-06-20 19:01:17 +0300

    v1.3
    - Updated function rendering the list of specific book category
    - Fixed some issues in css

--- src/js/best-sellers.js
+++ src/js/best-sellers.js
@@ -1,3 +1,4 @@
+import {fetchTopBooks} from './serviceApi.js'
 //This function recieves parsed data from api and returns the markup of book-list
 //at the home page. Should be used after any of serviceApi functions in async code.
 
@@ -9,48 +10,44 @@ export const renderBestSellersList = (data) => {
       .map((list) => {
         const { list_name: listName, books } = list;
         return `<div class="bookListWrapper"><h2 class="bestSellersTitle">${listName}</h2>
-   <ul class="bestSellersList">${books
-     .map((book) => {
-       const { _id: id, author, book_image, title } = book;
-       return `<li class="bestSellersListItem" data-id=${id}>
-            <div class="bestSellersThumb"><img class="bestSellersImage" src=${book_image} alt=${title}/></div>
-            <div class="bookDescriptionThumb"><h3 class="bookTitle">${
-              title.length > 18 ? title.slice(0, 18) + "..." : title
-            }</h3>
-            <p class="bookDescription">${author}</p></div>
-  
-        </li>`;
-     })
-     .join(
-       ""
-     )}</ul><button class="seeMoreBtn" type="button">See More</button></div>`;
+     <ul class="bestSellersList">${books
+       .map((book) => {
+         const { _id: id, author, book_image, title } = book;
+         return `<li class="bestSellersListItem" data-id=${id}>
+              <div class="bestSellersThumb"><img class="bestSellersImage" src=${book_image} alt=${title}/></div>
+              <div class="bookDescriptionThumb"><h3 class="bookTitle">${
+                title.length > 18 ? title.slice(0, 18) + "..." : title
+              }</h3>
+              <p class="bookDescription">${author}</p></div>
+    
+          </li>`;
+       })
+       .join(
+         ""
+       )}</ul><button class="seeMoreBtn" type="button">See More</button></div>`;
       })
       .join("")
   );
   return markup;
 };
 
-//The function below hasn't been completed yet because of backend malfunctions.
-//Don't export until it's done;
-const renderBooksByCategory = (data) => {
+//The function should be used to render a list of books of specific genre (category) 
+export const renderBooksByCategory = (data) => {
+  let categoryTitle;
   const markup = [...data]
-    .map((list) => {
-      const { list_name: listName, books } = list;
-      return `<h1>${listName}</h2>
- <ul class="bestSellersList">${books
-   .map((book) => {
-     const { _id: id, author, book_image, title } = book;
-     return `<li class="bestSellersListItem" data-id=${id}>
-          <div class="bestSellersThumb"><img class="bestSellersImage" src=${book_image} alt=${title}/></div>
-          <div class="bookDescriptionThumb"><h3 class="bookTitle">${
-            title.length > 18 ? title.slice(0, 18) + "..." : title
-          }</h3>
-          <p class="bookDescription">${author}</p></div>
-
-      </li>`;
-   })
-   .join("")}</ul>`;
+    .map((book) => {
+      const { _id: id, author, book_image, title, list_name: listName } = book;
+      categoryTitle = listName;
+      return `<li class="bookCategoryListItem" data-id=${id}>
+            <div class="bestSellersThumb"><img class="bestSellersImage" src=${book_image} alt=${title}/></div>
+            <div class="bookDescriptionThumb"><h3 class="bookTitle">${
+              title.length > 18 ? title.slice(0, 18) + "..." : title
+            }</h3>
+            <p class="bookDescription">${author}</p></div>
+        </li>`;
     })
     .join("");
-  return markup;
+
+  return `<h1 class="bestSellersHeader">${categoryTitle}</h1>
+     <ul class="bookCategoryList">${markup}</ul>`;
 };
