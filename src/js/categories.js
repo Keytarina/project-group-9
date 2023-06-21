eb88768edc544c85d312985a8d62843017f2eaa6
Enab13d <extendeddirection@gmail.com>
2023-06-21 11:26:18 +0300

    Update best-sellers and categories
    -Event listeners added for each list-item on categories list.
    - One of two callback functions being  invoked while clicking on such list-item, depending on it's text-content.

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,83 +1,59 @@
-import { fetchBooksCategoryList, fetchBooksByCategory } from './serviceApi.js';
+
+import { renderBooksByCategory, clearMarkup, renderBestSellersList } from './best-sellers.js';
+import { fetchBooksCategoryList,fetchBooksByCategory, fetchTopBooks } from './serviceApi.js'
 
 const category = document.querySelector(".categories-item");
-const list = document.querySelector('.categories-list');
-const gallery = document.querySelector('.categories');
+const list = document.querySelector('.categories-item');
+// const gallery = document.querySelector('.categories');
+const container = document.querySelector('#container-best');
 
-function markupList() {
-  fetchBooksCategoryList()
-    .then(data => {
-      const markupCategoriesList = data
-        .map(({ list_name }) => {
-          return `<li class="categories-list" id="${list_name}">${list_name}</li>`;
-        })
-        .join('');
-      category.insertAdjacentHTML('beforeend', markupCategoriesList);
-    })
-    .catch(error => console.log(error));
-}
-
-markupList();
-
-list.addEventListener('click', render);
-
-function render(books) {
-    if (!gallery) {
-    return;
-  } 
-  fetchBooksByCategory().then(book => {
-          const markupCategory = book.map((image) =>{
-      const {list_name,_id, book_image,author,title} = image;
-      return `
-      <ul class="render-category">
-        <li class="list-category" id="${_id}">
-          <div class="">
-              <img src="${book_image}" alt="${list_name}" loading="lazy" />
-              <div class="">
-              <h3 class="name-book"> ${title}</р>
-              <p class="category-author">${author}</p>
-            </div>
-          </div>
-        </li>
-      </ul>`;
-    }).join('');
-    gallery.insertAdjacentHTML('beforeend', markupCategory);
-  
-  const { height: imageHeight } = document
-    .querySelector('.categories')
-    .firstElementChild.getBoundingClientRect();
-    
-    window.scrollBy({
-    top: imageHeight * 2,
-    behavior: 'smooth',
-  });
-  }).catch (error => console.log(error));
+function markupList () {
+  fetchBooksCategoryList().then(data => {
+    const markupCategoriesList = data.map(({list_name: listName }) => {
+      return `<li class="categories-list" id=${listName}>${listName}</li>`;
+    }).join('')
+    category.insertAdjacentHTML('beforeend', markupCategoriesList);
+  }).catch((error) => console.log(error))
 };
 
+markupList();
+// -Event listeners added for each list-item on categories list.
 
+list.addEventListener('click', renderCategories);
+list.addEventListener('click', renderTopBooks);
 
-// const renderCategoriesList = (data) => {
+// - One of two callback functions below are being invoked while clicking on such list-item, depending on it's text-content.
+async function renderCategories(e) {
+  if (e.target.nodeName !== "LI" && e.target.textContent === "All categories") {
+    return;
+  }
+    try {
+      const data = await fetchBooksByCategory(e.target.textContent);
+      const markup = renderBooksByCategory(data);
+      clearMarkup();
+      container.insertAdjacentHTML('beforeend', markup);
   
-//   console.log(fetchBooksByCategory());
-//     const renderCategoryList = header.concat([...data].map((list) => {
-//         const { list_name: listName, books } = list;
-//         return `<div class="bookListWrapper"><h2 class="bestSellersTitle">${listName}</h2>
-//             <ul class="bestSellersList">${books
-//             .map((book) => {
-//     const { _id: id, author, book_image, title } = book;
-//       return `<li class="bestSellersListItem" data-id=${id}>
-//                 <div class="bestSellersThumb">
-//                 <img class="bestSellersImage" src=${book_image} alt=${title}/>
-//                 </div>
-//                 <div class="bookDescriptionThumb"><h3 class="bookTitle">${
-//                   title.length > 18 ? title.slice(0, 18) + "..." : title
-//                 }</h3>
-//                 <p class="bookDescription">${author}</p></div>
-//               </li>`;
-//         }).join("")}
-//         </ul>`
-//       }).join(""));
-//   return renderCategoryList;
-// };
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function renderTopBooks (e) {
+  if (e.target.textContent === "All categories") {
+    try {
+      const data = await fetchTopBooks();
+      const markup = renderBestSellersList(data);
+      clearMarkup();
+      container.insertAdjacentHTML('beforeend', markup);
+    } catch (error) {
+      console.log(error);
+    }
+}
+  }
 
-// renderCategoriesList();
+  
+// const { height: imageHeight } = container.firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+// top: imageHeight * 2,
+// behavior: 'smooth',
+// });
