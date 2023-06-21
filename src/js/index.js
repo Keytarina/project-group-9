c94395ea9fa87ce24644e219f2e6977a76b10456
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2023-06-22 01:03:59 +0300

    Update path

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
 import './serviceApi';
 import './theme.js';
+import './modal';
+
 import './header';
+import './categories';
 import './support';
+import './books';
+
+import './best-sellers';
+// import './shopping-list';
+import './btn-see-more';
