19ddc3c3c58c74fc78996c34f4a993a156a154b3
Lovchinskaya <33342414+Lovchinskaya@users.noreply.github.com>
2023-06-22 07:39:14 +0300

    theme cross colors
    1. header color background fixed
    2. best sellers text color + see more button colors

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 import './serviceApi';
 import './theme.js';
-// import './modal';
+//import './modal';
 
 import './header';
-// import './categories';
-import './support';
-// import './books';
+ //import './categories';
+//import './support';
+import './books';
 import './authorization';
-// import './best-sellers';
+ //import './best-sellers';
 // import './shopping-list';
 // import './btn-see-more';
