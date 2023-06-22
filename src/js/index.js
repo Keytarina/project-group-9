42c9346909a165da456a900bf51424e8a764bf40
Sergii <svbulgakov75@gmail.com>
2023-06-22 08:03:22 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 import './serviceApi';
 import './theme.js';
-// import './modal';
+import './modal';
 
 import './header';
-// import './categories';
+import './categories';
 import './support';
-// import './books';
+import './books';
 import './authorization';
-// import './best-sellers';
-// import './shopping-list';
-// import './btn-see-more';
+import './best-sellers';
+import './shopping-list';
+import './btn-see-more';
