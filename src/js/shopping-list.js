ba316abecc6eae9b2655cad25dc4aff372a9ef92
sgrischenko8 <sgrischenko8@gmail.com>
2023-06-23 00:19:48 +0300

    fix conflict with eventListener

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -470,7 +470,6 @@ function deleteBook(event) {
     event.target.nodeName === 'svg' ||
     event.target.nodeName === 'BUTTON'
   ) {
-    console.log('but');
     procedureDeletingBook(event);
   }
   return;
