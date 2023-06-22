8db7870eb1b3f9723668345242c22e6b9afee73e
sgrischenko8 <120989472+sgrischenko8@users.noreply.github.com>
2023-06-22 12:06:57 +0300

    Update shopping-list.js

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -9,7 +9,9 @@ const secondBtn = document.querySelector('button[data-page="second"]');
 const thirdBtn = document.querySelector('.btn-hidden');
 
 const mediaQuery = window.matchMedia('(min-width: 768px)');
+const mediaQuerySupport = window.matchMedia('(max-width: 1439px)');
 const support = document.querySelector('.support');
+support.style.position = "static");
 if (mediaQuery.matches) {support.classList.add('visually-hidden')  }
 
 // Імітація наповнення LocalStorage id-шниками книг -------
