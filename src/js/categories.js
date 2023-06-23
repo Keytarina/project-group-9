2aacf6f7a6eb471dc65b6cbec1d348e5cb070aab
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-06-23 18:21:15 +0300

    fixbugs

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,23 +1,32 @@
+import {
+  renderBooksByCategory,
+  clearMarkup,
+  renderBestSellersList,
+} from './best-sellers.js';
+import {
+  fetchBooksCategoryList,
+  fetchBooksByCategory,
+  fetchTopBooks,
+} from './serviceApi.js';
 
-import { renderBooksByCategory, clearMarkup, renderBestSellersList } from './best-sellers.js';
-import { fetchBooksCategoryList,fetchBooksByCategory, fetchTopBooks } from './serviceApi.js'
-
-const category = document.querySelector(".categories-item");
+const category = document.querySelector('.categories-item');
 const list = document.querySelector('.categories-item');
 const color = document.querySelector('.categories-list');
 const container = document.querySelector('#container-best');
 
-
-
-function markupList () {
-  fetchBooksCategoryList().then(data => {
-    const markupCategoriesList = data.map(({list_name: listName }) => {
-      return `<li class="categories-list" id=${listName}>${listName}</li>`;
-    }).join('')
-    category.insertAdjacentHTML('beforeend', markupCategoriesList);
-    changeColor();
-  }).catch((error) => console.log(error))
-};
+function markupList() {
+  fetchBooksCategoryList()
+    .then(data => {
+      const markupCategoriesList = data
+        .map(({ list_name: listName }) => {
+          return `<li class="categories-list" id=${listName}>${listName}</li>`;
+        })
+        .join('');
+      category.insertAdjacentHTML('beforeend', markupCategoriesList);
+      // changeColor();
+    })
+    .catch(error => console.log(error));
+}
 
 markupList();
 // -Event listeners added for each list-item on categories list.
@@ -27,36 +36,33 @@ list.addEventListener('click', renderTopBooks);
 
 // - One of two callback functions below are being invoked while clicking on such list-item, depending on it's text-content.
 async function renderCategories(e) {
-  if (e.target.nodeName !== "LI" && e.target.textContent === "All categories") {
+  if (e.target.nodeName !== 'LI' && e.target.textContent === 'All categories') {
     return;
   }
-    try {
-      const data = await fetchBooksByCategory(e.target.textContent);
-      const markup = renderBooksByCategory(data);
-      clearMarkup();
-      container.insertAdjacentHTML('beforeend', markup);
-  
-    }
-    catch (error) {
+  try {
+    const data = await fetchBooksByCategory(e.target.textContent);
+    const markup = renderBooksByCategory(data);
+    clearMarkup();
+    container.insertAdjacentHTML('beforeend', markup);
+  } catch (error) {
     console.log(error);
-  };
-};
+  }
+}
 
-async function renderTopBooks (e) {
-  if (e.target.textContent === "All categories") {
+async function renderTopBooks(e) {
+  if (e.target.textContent === 'All categories') {
     try {
       const data = await fetchTopBooks();
       const markup = renderBestSellersList(data);
       clearMarkup();
       container.insertAdjacentHTML('beforeend', markup);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
-  };
-};
+  }
+}
 
-function changeColor() {
-  color.forEach(e => e.classList.remove('activies'));
-  this.classList.add('activies');
-};
+// function changeColor() {
+//   color.forEach(e => e.classList.remove('activies'));
+//   this.classList.add('activies');
+// };
