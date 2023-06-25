8ef55be522cffa4656c2c68d6f9260a1a06205b3
sgrischenko8 <120989472+sgrischenko8@users.noreply.github.com>
2023-06-25 21:20:53 +0300

    Debug/current page (#116)
    Update shopping-list.js, header.js

--- src/js/header.js
+++ src/js/header.js
@@ -21,6 +21,8 @@ const headerHome = document.querySelector('.header-home');
 const headerShoppingList = document.querySelector('.header-shopping-list');
 const logoType = document.querySelector('.header-logo');
 
+console.log(document.lastElementChild.baseURI);
+
 if (
   document.lastElementChild.baseURI ===
   'https://keytarina.github.io/project-group-9/index.html'
